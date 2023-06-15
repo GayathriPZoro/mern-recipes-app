66df171a02d45dc1240ac251615b185aeecf3367
Gayathri Polubothu <97259893+gayathri-polubothu@users.noreply.github.com>
2023-06-15 15:24:31 +0100

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -15,6 +15,6 @@ app.use('/recipes', recipesRouter);
 
 const mongodb_connect_url = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@recipes-app-cluster.ujp7f4d.mongodb.net/my-recipes-app?retryWrites=true&w=majority`
 const mongodb_connect_url2 = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@recipes.cbjcm98.mongodb.net/recipes?retryWrites=true&w=majority`
-mongoose.connect(mongodb_connect_url)
+mongoose.connect(process.env.MONGODB_URL)
 
 app.listen(3040, () => console.log('Server has started'))
