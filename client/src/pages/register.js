ceb283e2e07c891385379a1ffb89c97bbc6c211a
Gayathri Polubothu <97259893+gayathri-polubothu@users.noreply.github.com>
2023-06-15 14:07:08 +0100

    Client folder added

---
+++ client/src/pages/register.js
@@ -0,0 +1,48 @@
+import {useState} from "react";
+import {AuthForm} from "./authForm";
+import * as Services from "../services/services";
+import {MuiSnackbar} from "../utils/MuiSnackbar";
+
+export const Register = () => {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [email, setEmail] = useState('')
+    const [open, setOpen] = useState(false);
+    const [data, setData] = useState({})
+
+
+    const handleSubmit = async(event) => {
+        event.preventDefault();
+        const response = await Services.registerUser({username, password, email})
+        if(response.error) {
+           setData({
+                   message:'Registration Failed!',
+                   severity: 'error'
+               }
+           )
+        }else {
+            setData({
+                    message: response.message || 'Registration Successful!',
+                    severity: 'success'
+                }
+            )
+        }
+        setOpen(true)
+        setUsername('');
+        setPassword('')
+        setEmail('')
+    }
+    return (
+        <>
+            <AuthForm
+                username={username} password={password}
+                setUsername={setUsername} setPassword={setPassword}
+                email={email} setEmail={setEmail} label={'Register'}
+                handleSubmit={handleSubmit}
+            />
+            {open && (
+                <MuiSnackbar severity={data.severity} message={data.message} setOpen={setOpen} open={true}/>
+            )}
+        </>
+    )
+}
