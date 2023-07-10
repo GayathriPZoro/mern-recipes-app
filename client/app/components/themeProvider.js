0eeba34cc8f19a573d2ea7f3bc8d45bd6b5c11af
Gayathri Polubothu <97259893+gayathri-polubothu@users.noreply.github.com>
2023-07-10 12:59:23 +0100

    Added loading spinner when navigating between pages

--- client/app/components/themeProvider.js
+++ client/app/components/themeProvider.js
@@ -1,8 +1,29 @@
 import {ThemeProvider} from "@mui/material/styles";
 import themeConfigs from "../config/theme.configs";
 import {useThemeContext} from "../context/theme";
+import {useGlobalLoadingContext} from "../context/globalLoading";
+import {useRouter} from "next/router";
 const MyThemeProvider = ({children}) => {
     const {themeMode} = useThemeContext()
+    const {setGlobalLoading} = useGlobalLoadingContext()
+    const router=useRouter()
+    const handleRouteChange = (url, { shallow }) => {
+        console.log(
+            `App is changing to ${url} ${
+                shallow ? 'with' : 'without'
+            } shallow routing`
+        )
+        setGlobalLoading(true)
+        return;
+    };
+
+    const handleRouteComplete = (url, { shallow }) => {
+        setGlobalLoading(false)
+        return;
+    };
+
+    router?.events?.on('routeChangeStart', handleRouteChange)
+    router?.events?.on('routeChangeComplete', handleRouteComplete)
     return(
         <ThemeProvider theme={themeConfigs.custom({ mode: themeMode })}>
             {children}
